ef566a792ef9a5e4039a5838ab4300d82f2b8b5c
saifAlii <s.a.sunny19156@gmail.com>
2023-03-03 20:39:34 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include<string.h>
 #include<math.h>
 using namespace std;
-
 int matrix1[4][4] = { 1, 0, 3, 2, 3, 2, 1, 0, 0, 2, 1, 3, 3, 1, 3, 2 };
 int matrix2[4][4] = { 0, 1, 2, 3, 2, 0, 1, 3, 3, 0, 1, 0, 2, 1, 0, 3 };
 struct Keys 
